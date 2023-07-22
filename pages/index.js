af7a7ab5443cd02a5126ab2a96bf7eea969992d9
AudiHagi <audi.hagi2002@gmail.com>
2023-07-22 11:52:53 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,110 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import Image from 'next/image';
+import avatar from '../public/avatar.png'
+import ubm from '../public/ubm.png'
+import bangkit from '../public/bangkit.jpg'
+import web1 from '../public/web1.png'
+import web2 from '../public/web2.png'
+import web3 from '../public/web3.png'
+import web4 from '../public/web4.png'
+import web5 from '../public/web5.png'
+import web6 from '../public/web6.png'
+import { useState } from 'react';
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <Head>
+        <title>Audi Hagi Portofolio</title>
+        <meta></meta>
+        <link></link>
+      </Head>
+      <main className="bg-white px-10 md:px-20 lg:px-40">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="min-h-screen flex flex-col justify-center items-center">
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Audi Hagi</h2>
+            <h3 className="text-2xl py-2 md:text-3xl">Android and Website Development</h3>
+            <p className="text-md leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">Informatics Undergraduate Student</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 text-gray-600">
+            <a href="https://www.linkedin.com/in/audihagi">
+              <AiFillLinkedin />
+            </a>
+            <a href="https://github.com/AudiHagi">
+              <AiFillGithub />
+            </a>
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="min-h-screen">
+          <div>
+            <h3 className="text-3xl py-1">Education Background</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 text-justify">
+              I am currently pursuing a Bachelor's degree in
+              <span className="text-teal-500"> Informatics at Bunda Mulia University, </span>
+              and I am in my seventh semester of study. As part of my academic journey, I have had the privilege of participating in an independent study program at
+              <span className="text-teal-500"> Bangkit Academy by Google, GoTo, and Traveloka. </span>
+              Throughout this program, I focused on the Android/Mobile Development learning path, which provided me with invaluable knowledge and skills in this dynamic and fast-evolving field.
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-center gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image className="mx-auto" src={ubm} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">Bunda Mulia University</h3>
+              <p className="py-2">Informatics</p>
+              <h4 className="py-4 text-teal-600">Skills</h4>
+              <p className="text-gray-800 py-1">UI/UX Design</p>
+              <p className="text-gray-800 py-1">Web Development</p>
+              <p className="text-gray-800 py-1">Mobile development (Java)</p>
+              <p className="text-gray-800 py-1">Data Structures</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image className="mx-auto" src={bangkit} width={100} height={100} />
+              <h3 className="text-lg font-medium pt- pb-2">Bangkit Academy 2023</h3>
+              <p className="py-2">Android Learning Path</p>
+              <h4 className="py-4 text-teal-600">Skills</h4>
+              <p className="text-gray-800 py-1">Mobile development (Kotlin)</p>
+              <p className="text-gray-800 py-1">UI/UX Design</p>
+              <p className="text-gray-800 py-1">SOLID Principles</p>
+              <p className="text-gray-800 py-1">Version Control System</p>
+            </div>
+          </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        <section className="min-h-screen">
+          <div>
+            <h3 className="text-3xl py-1">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 text-justify">
+              I specialize in Android Development, leveraging my expertise in creating cutting-edge mobile applications. With a strong foundation in Java and Kotlin, I craft user-friendly and intuitive interfaces, emphasizing seamless user experiences. Throughout my academic journey and participation in industry-driven programs like Bangkit Academy, powered by tech leaders such as Google, GoTo, and Traveloka, I have honed my skills in building innovative and robust Android apps. My passion for exploring the latest technologies and trends drives me to deliver high-quality solutions that leave a positive impact on users. I am committed to continuous learning and eagerly embrace new challenges to remain at the forefront of the dynamic Android development landscape. Through my portfolio, I aim to showcase my diverse range of Android projects, demonstrating my dedication to creating exceptional mobile experiences.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <Image src={web1} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" />
+            </div>
+            <div className="basis-1/3 flex-1 ">
+              <Image src={web2} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" />
+            </div>
+            <div className="basis-1/3 flex-1 ">
+              <Image src={web3} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" />
+            </div>
+            <div className="basis-1/3 flex-1 ">
+              <Image src={web4} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" />
+            </div>
+            <div className="basis-1/3 flex-1 ">
+              <Image src={web5} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" />
+            </div>
+            <div className="basis-1/3 flex-1 ">
+              <Image src={web6} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" />
+            </div>
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div >
+  );
 }
