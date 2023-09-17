a42238b3e59c93029f76ef746f6e9d51542ab84f
AudiHagi <audi.hagi2002@gmail.com>
2023-09-17 07:44:54 +0700

    insert some comment

--- pages/About.js
+++ pages/About.js
@@ -4,15 +4,15 @@ export default function About() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center">
       <div className="text-center p-10">
-        <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+        <h1 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
           Audi Hagi
-        </h2>
+        </h1>
         <h3 className="text-2xl py-2 md:text-3xl">
           Android and Website Development
         </h3>
-        <p className="text-md leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+        {/* <p className="text-md leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
           Informatics Undergraduate Student
-        </p>
+        </p> */}
       </div>
       <div className="text-5xl flex justify-center gap-16 text-gray-600">
         <a href="https://www.linkedin.com/in/audihagi">
